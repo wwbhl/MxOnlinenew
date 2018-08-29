bbf7c72b77449625e63abc9f85585cfeea59b5a4
baohailong <baohailong@sina.com>
2018-08-29 22:29:35 +0800

    add users and courses adminx

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -12,7 +12,7 @@ class Course(models.Model):
     name = models.CharField(max_length=50, verbose_name=u'课程名')
     desc = models.CharField(max_length=300, verbose_name=u'课程描述')
     detail = models.TextField(verbose_name=u'课程详情')
-    degree = models.CharField(choices=(('primary', '初级'), ('middle', '中级'), ('advanced', '高级')), max_length=2)
+    degree = models.CharField(verbose_name=u'难度', choices=(('primary', '初级'), ('middle', '中级'), ('advanced', '高级')), max_length=8)
     learn_times = models.IntegerField(default=0, verbose_name=u'学习时长（分钟数）')
     students = models.IntegerField(default=0, verbose_name=u'学习人数')
     fav_num = models.IntegerField(default=0, verbose_name=u'收藏人数')
@@ -24,6 +24,8 @@ class Course(models.Model):
         verbose_name = u'课程'
         verbose_name_plural = verbose_name
 
+    def __unicode__(self):
+        return self.name
 
 class Lesson(models.Model):
     course = models.ForeignKey(Course, verbose_name=u'课程')
