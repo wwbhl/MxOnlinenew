e217991fb8decac5da30086c045248750eb70511
WeatherBaren <baohailong@sina.com>
2018-08-29 08:49:07 +0800

    config mysql

--- MxOnline/settings.py
+++ MxOnline/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'MxOnline.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mxonline',
+        'USER': 'test',
+        'PASSWORD': 'test123',
+        'HOST': '127.0.0.1',
+        'PORT': '3306'
     }
 }
 
