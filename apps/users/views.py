be0a47376f5cc2d9e9a8cc11d8ece7fc64a2bfc9
WeatherBaren <baohailong@sina.com>
2018-08-30 15:36:28 +0800

    use email and username login

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,6 +1,8 @@
+# -*- encoding:utf-8 -*-
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.backends import ModelBackend
+from django.db.models import Q
 
 from .models import UserProfile
 # Create your views here.
@@ -9,7 +11,7 @@ from .models import UserProfile
 class CustomBackend(ModelBackend):
     def authenticate(self, username=None, password=None, **kwargs):
         try:
-            user = UserProfile.objects.get(username=username)
+            user = UserProfile.objects.get(Q(username=username)|Q(email=username))
             if user.check_password(password):
                 return user
         except Exception as e:
@@ -24,6 +26,6 @@ def user_login(request):
             login(request, user)
             return render(request, 'index.html')
         else:
-            return render(request, 'login.html', {})
+            return render(request, 'login.html', {'msg':'用户名或密码错误!'})
     elif request.method == "GET":
         return  render(request, 'login.html', {})
