412fb6fef1ad0a2b852aa0cbcfe3596852f30d04
baohailong <baohailong@sina.com>
2018-09-04 23:23:47 +0800

    fav finsh

--- apps/organization/urls.py
+++ apps/organization/urls.py
@@ -4,7 +4,7 @@ __author__ = 'baohailong'
 __data__ = '2018/9/3 8:40 PM'
 
 from django.conf.urls import url, include
-from .views import OrgView, AddUserAskView, OrgHomeView, OrgCourseView, OrgCDescView, OrgTeacherView
+from .views import OrgView, AddUserAskView, OrgHomeView, OrgCourseView, OrgCDescView, OrgTeacherView, AddFavView
 
 urlpatterns = [
     #课程机构列表页
@@ -14,4 +14,7 @@ urlpatterns = [
     url(r'^course/(?P<org_id>\d+)/$', OrgCourseView.as_view(), name='org_course'),
     url(r'^desc/(?P<org_id>\d+)/$', OrgCDescView.as_view(), name='org_desc'),
     url(r'^teacher/(?P<org_id>\d+)/$', OrgTeacherView.as_view(), name='org_teacher'),
+    #机构收藏
+    url(r'^add_fav/$', AddFavView.as_view(), name='add_fav'),
+
 ]
