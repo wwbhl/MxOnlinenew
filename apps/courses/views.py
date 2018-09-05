bdba3101aabc6294f1e272bb98732f17e4f1d1e0
WeatherBaren <baohailong@sina.com>
2018-09-05 13:54:00 +0800

    courses list

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -9,7 +9,17 @@ from .models import Course
 
 class CourseListView(View):
     def get(self, request):
-        all_courses = Course.objects.all()
+        all_courses = Course.objects.all().order_by('-add_time')
+        hot_courses = Course.objects.all().order_by('-click_nums')[:3]
+
+        #课程排序
+        sort = request.GET.get('sort', '')
+        if sort:
+            if sort == 'students':
+                all_courses = all_courses.order_by('-students')
+            elif sort == 'hot':
+                all_courses = all_courses.order_by('-click_nums')
+
         # 对课程进行分页
         try:
             page = request.GET.get('page', 1)
@@ -23,4 +33,6 @@ class CourseListView(View):
         courses = p.page(page)
         return render(request, 'course-list.html', {
             'all_courses':courses,
+            'sort':sort,
+            'hot_courses':hot_courses,
         })
