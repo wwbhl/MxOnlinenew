e9e2952590f74c3a05ee8a0dc4f91fa2774bd47c
WeatherBaren <baohailong@sina.com>
2018-09-05 12:09:43 +0800

    courses pages

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -1,3 +1,26 @@
+# -*- encoding:utf-8 -*-
 from django.shortcuts import render
+from django.views.generic import View
+from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 
+from .models import Course
 # Create your views here.
+
+
+class CourseListView(View):
+    def get(self, request):
+        all_courses = Course.objects.all()
+        # 对课程进行分页
+        try:
+            page = request.GET.get('page', 1)
+        except PageNotAnInteger:
+            page = 1
+
+        # Provide Paginator with the request object for complete querystring generation
+
+        p = Paginator(all_courses, 3, request=request)
+
+        courses = p.page(page)
+        return render(request, 'course-list.html', {
+            'all_courses':courses,
+        })
