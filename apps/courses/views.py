2800d69ac29471488388b3d4c25307ec39565b2f
WeatherBaren <baohailong@sina.com>
2018-09-06 14:11:22 +0800

    course commend and login class

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -5,7 +5,8 @@ from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse
 
 from .models import Course, CourseResource
-from operation.models import UserFavorite, CourseComments
+from operation.models import UserFavorite, CourseComments, UserCourse
+from utils.mixin_utils import LoginRequiredMixin
 # Create your views here.
 
 
@@ -72,20 +73,34 @@ class CourseDetailView(View):
         })
 
 
-class CourseInfoView(View):
+class CourseInfoView(LoginRequiredMixin, View):
     '''
     课程章节信息
     '''
     def get(self, request, course_id):
         course = Course.objects.get(id=int(course_id))
+        #查询用户是否已经关联了该课程
+        user_courses = UserCourse.objects.filter(user=request.user, course=course)
+        if not user_courses:
+            user_course = UserCourse(user=request.user, course=course)
+            user_course.save()
+
+        user_courses = UserCourse.objects.filter(course=course)
+        user_ids = [user_course.user.id for user_course in user_courses]
+        all_user_courses = UserCourse.objects.filter(user_id__in=user_ids)
+        #取出所有课程 id
+        course_ids = [user_course.course.id for user_course in all_user_courses]
+        #获取学过该用户学过其他的所有课程
+        relate_courses = Course.objects.filter(id__in=course_ids).order_by('-click_nums')[:5]
         all_resources = CourseResource.objects.filter(course=course)
         return render(request, 'course-video.html', {
             'course': course,
             'course_resources': all_resources,
+            'relate_courses':relate_courses,
         })
 
 
-class CommentsView(View):
+class CommentsView(LoginRequiredMixin, View):
     '''
     课程章节信息
     '''
