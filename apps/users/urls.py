909395fa8bfab7cef806db6cbd5b41b9d10d5dd3
WeatherBaren <baohailong@sina.com>
2018-09-10 13:45:53 +0800

    modify user password

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -4,11 +4,13 @@ __author__ = 'baohailong'
 __data__ = '2018/9/7 5:01 PM'
 
 from django.conf.urls import url, include
-from .views import UserInfoView, UploadImageView
+from .views import UserInfoView, UploadImageView, UpdatePwdView
 
 urlpatterns = [
     #用户信息
     url(r'^info/$', UserInfoView.as_view(), name='user_info'),
     #用户头像上传
     url(r'^image/upload/$', UploadImageView.as_view(), name='image_upload'),
+    #用户个人中心修改密码
+    url(r'^update/pwd/$', UpdatePwdView.as_view(), name='update_pwd'),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,4 +1,6 @@
 # -*- encoding:utf-8 -*-
+import json
+
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.backends import ModelBackend
@@ -155,3 +157,24 @@ class UploadImageView(LoginRequiredMixin, View):
             return HttpResponse('{"status":"success"}', content_type='application/json')
         else:
             return HttpResponse('{"status":"fail"}', content_type='application/json')
+
+
+class UpdatePwdView(View):
+    '''
+    个人中心修改用户密码
+    '''
+    def post(self, request):
+        modify_form = ModifyPwdForm(request.POST)
+        if modify_form.is_valid():
+            pwd1 = request.POST.get('password1', '')
+            pwd2 = request.POST.get('password2', '')
+            if pwd1 != pwd2:
+                return HttpResponse('{"status":"fail", "msg":"密码不一致"}', content_type='application/json')
+            user = request.user
+            user.password = make_password(pwd2)
+            user.save()
+
+            return HttpResponse('{"status":"success"}', content_type='application/json')
+
+        else:
+            return HttpResponse(json.dumps(modify_form.errors), content_type='application/json')
